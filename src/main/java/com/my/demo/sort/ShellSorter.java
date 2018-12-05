f3d43c71f0c003c46b82962e84fa5dd6af47d489
shiyongbiao@incar100.com <shiyongbiao@incar100.com>
2018-12-05 19:39:10 +0800

    '实现希尔排序'

--- src/main/java/com/my/demo/sort/ShellSorter.java
+++ src/main/java/com/my/demo/sort/ShellSorter.java
@@ -5,9 +5,36 @@ public class ShellSorter implements Sorter {
     public String name() {
         return "希尔排序";
     }
-    
+
     @Override
     public int sort(int[] array, int fromIndex, int toIndex) {
-        return 0;
+
+        int len = toIndex - fromIndex;
+        int gap = 1;
+        while (gap < len / 3) {
+            gap = gap * 3 + 1; // 动态定义间隔
+        }
+
+        int count = 0;
+        for (; gap > 0; gap = gap / 3) {
+            for (int i = fromIndex + gap; i < toIndex; i++) {
+                int current = array[i];
+                for (int j = i - gap; j >= fromIndex; j -= gap) {
+                    count++;
+                    if (array[j] < current) {
+                        array[j + gap] = current;
+                        break;
+                    } else {
+                        array[j + gap] = array[j];
+                        if (j - fromIndex <= gap) {
+                            array[j] = current;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
     }
+
 }
